3546fd8753f93904ecdfddf9e0c4535691fe1157
eunjin <eunjinfizz@gmail.com>
2021-02-22 23:21:00 +0900

    2021-02-22

---
+++ src/baekjoon/Main_2798.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_2798 {
+//세 장의 카드를 골라 M에 최대한 가까운 합을 구해 출력하는 문제
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); //카드의 개수
+		int M = Integer.parseInt(st.nextToken()); //최대한 가깝게 만들어야하는 수
+		
+		String[] cards = br.readLine().split(" ");
+		
+		
+
+	}
+
+}
